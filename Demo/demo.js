ca86d7847b34f59aa396ed4eab07b0a0de64b882
Vincent <vincent.diep61@gmail.com>
2014-05-07 03:07:25 -0400

    Update demo

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,34 +1,17 @@
 $(document).ready(function() {
     $('.timepicker', '#demo-basic').qcTimepicker();
 
-    $('.timepicker', '#demo-custom-range').qcTimepicker({
-        minTime: '8:30',
-        maxTime: '15:30'
-    });
+    $('.timepicker', '#demo-custom-range').qcTimepicker();
 
-    $('.timepicker', '#demo-custom-intervals').qcTimepicker({
-        minTime: '8:30',
-        maxTime: '15:30',
-        step: '0:90'
-    });
+    $('.timepicker', '#demo-custom-intervals').qcTimepicker();
 
     $('#demo-custom-formats-1').qcTimepicker({
-        minTime: '8:30',
-        maxTime: '15:30',
-        step: '0:90',
         format: 'h:mm a'
     });
 
     $('#demo-custom-formats-2').qcTimepicker({
-        minTime: '8:30',
-        maxTime: '15:30',
-        step: '0:90',
         format: 'h:mm:ss a'
     });
 
-    $('.timepicker', '#demo-placeholder').qcTimepicker({
-        minTime: '8:30',
-        maxTime: '15:30',
-        step: '0:90'
-    });
+    $('.timepicker', '#demo-placeholder').qcTimepicker();
 });
